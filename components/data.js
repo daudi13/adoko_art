6c9f01bba710273d9002a455ba4983236b61d34f
daudi13 <davouma13@gmail.com>
2023-04-07 21:27:01 +0300

    Add Data modules

---
+++ components/data.js
@@ -0,0 +1,57 @@
+import img1 from "../public/images/boston-public-library-_f9cP4_unmg-unsplash.jpg"
+import img2 from "../public/images/europeana-5TK1F5VfdIk-unsplash.jpg"
+import img3 from "../public/images/europeana-6c43FgRt0Dw-unsplash.jpg"
+import img4 from "../public/images/tamara-menzi-n-vnWQmmVoY-unsplash.jpg"
+import img5 from "../public/images/luca-nicoletti-O8CHmj0zgAg-unsplash.jpg"
+import img6 from "../public/images/geordanna-cordero-2Qg4y32pdCc-unsplash.jpg"
+import img7 from "../public/images/birmingham-museums-trust-e0wBK0xJXYQ-unsplash.jpg"
+
+
+const data = [
+  {
+    id: 1,
+    image: img1,
+    desc: "This delightful painting captures the joy and freedom of nature, with a group of brightly colored birds perched on a tree branch. The birds are a riot of color, with feathers of blue, green, yellow, and red. They are depicted in mid-flight, as if frozen in time, with their wings outstretched in a moment of pure freedom. The tree branch is intricately detailed, with gnarled wood and delicate leaves adding texture to the painting. The overall effect is one of whimsy and happiness, capturing the spirit of nature in all its glory.", 
+    dimensions: "100cm x 70cm"
+  },
+  {
+    id: 2,
+    image: img2,
+    desc: "In this mesmerizing painting, a gorgeous bouquet of vibrant flowers is on display against a serene background. The colors of the flowers burst forth from the canvas, with shades of pink, yellow, orange, and red creating a beautiful tapestry of petals. The flowers are arranged in a way that draws the eye towards the center of the painting, where a particularly striking bloom takes center stage. The background is a tranquil sea of blue, with just a hint of greenery peeking through the corners. The overall effect is one of peacefulness and natural beauty.",
+    dimensions: "120cm X 80cm",
+  },
+  {
+    id: 3,
+    image: img3,
+    desc: "In this mesmerizing painting, a gorgeous bouquet of vibrant flowers is on display against a serene background. The colors of the flowers burst forth from the canvas, with shades of pink, yellow, orange, and red creating a beautiful tapestry of petals. The flowers are arranged in a way that draws the eye towards the center of the painting, where a particularly striking bloom takes center stage. The background is a tranquil sea of blue, with just a hint of greenery peeking through the corners. The overall effect is one of peacefulness and natural beauty.",
+    dimensions: "120cm X 80cm",
+  },
+  {
+    id: 4,
+    image: img4,
+    desc: "This brooding painting depicts a stormy sky, with dark clouds gathering ominously overhead. The sky is a swirling mass of greys and blacks, with flashes of lightning illuminating the darkness. The horizon is barely visible, shrouded in a misty haze that adds to the sense of foreboding. The painting is moody and atmospheric, capturing the raw power of nature and the fleeting beauty of a passing storm.",
+    dimensions: "120cm X 80cm",
+  },
+  {
+    id: 5,
+    image: img5,
+    desc: "This brooding painting depicts a stormy sky, with dark clouds gathering ominously overhead. The sky is a swirling mass of greys and blacks, with flashes of lightning illuminating the darkness. The horizon is barely visible, shrouded in a misty haze that adds to the sense of foreboding. The painting is moody and atmospheric, capturing the raw power of nature and the fleeting beauty of a passing storm.",
+    dimensions: "120cm X 80cm",
+  },
+  {
+    id: 6,
+    image: img6,
+    desc: "This brooding painting depicts a stormy sky, with dark clouds gathering ominously overhead. The sky is a swirling mass of greys and blacks, with flashes of lightning illuminating the darkness. The horizon is barely visible, shrouded in a misty haze that adds to the sense of foreboding. The painting is moody and atmospheric, capturing the raw power of nature and the fleeting beauty of a passing storm.",
+    dimensions: "120cm X 80cm",
+  },
+  {
+    id:7,
+    image: img7,
+    desc: "This brooding painting depicts a stormy sky, with dark clouds gathering ominously overhead. The sky is a swirling mass of greys and blacks, with flashes of lightning illuminating the darkness. The horizon is barely visible, shrouded in a misty haze that adds to the sense of foreboding. The painting is moody and atmospheric, capturing the raw power of nature and the fleeting beauty of a passing storm.",
+    dimensions: "120cm X 80cm",
+  },
+]
+
+export default data;
+
+console.log(data)
