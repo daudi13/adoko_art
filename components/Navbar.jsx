fd66977b1f269ad34f0144c4d9824e9d456f3086
daudi13 <davouma13@gmail.com>
2023-04-07 19:27:06 +0300

    Add Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from "@/styles/Navbar.module.scss"
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.nav}>
+      <h1 className={styles.logo}>Adoko Arts</h1>
+      <div className={styles.navSection}>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}><Link href="" passHref>Home</Link></li>
+          <li className={styles.navItem}><Link href="" passHref>Shop</Link></li>
+          <li className={styles.navItem}><Link href="" passHref>Events</Link></li>
+          <li className={styles.navItem}><Link href="" passHref>About me</Link></li>
+          <li className={styles.navItem}><Link href="" passHref>Contacts</Link></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
