225b9ffbb9c8d1fb157040034d6aa3e9b7d12dba
daudi13 <davouma13@gmail.com>
2023-04-08 14:14:30 +0300

    Add art desc

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -7,6 +7,9 @@ const ItemCard = ({ cardInfo }) => {
     <div className={styles.cardImage}>
       <Image src={Img} alt="painting" width="450" height="600" className={styles.img} />
       <button className={styles.infoButton}>info</button>
+      <div className={styles.desc}>
+        <p>{cardInfo.desc}</p>
+      </div>
     </div>
   )
 }
