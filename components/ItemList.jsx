88e4e203b6ed0a6f7a12c06ca018448939e7fe7e
daudi13 <davouma13@gmail.com>
2023-04-08 13:48:26 +0300

    Add styling

--- components/ItemList.jsx
+++ components/ItemList.jsx
@@ -3,23 +3,24 @@ import data from "./data"
 import ItemCard from "./ItemCard"
 
 const ItemList = () => {
-  console.log(data)
   const newData = data
   return (
     <div className={styles.container}>
-      {
-        newData?.map((cardInfo) => {
-          return (
-            <ItemCard cardInfo={cardInfo} key={cardInfo.id} />
-          )
-        })
-      }
-      <div className={styles.desc}>
-        <p>Meet John, a talented Kenyan artist who embarked on a creative journey two years ago that resulted in a stunning project that captivated art enthusiasts across the country. Inspired by his personal experiences and cultural background, John sought to create a collection of paintings that would not only showcase his artistic abilities but also convey a powerful message about the importance of preserving African traditions and identity.</p>
-        <p>John's project, titled "Echoes of the Ancestors," was a culmination of months of hard work and dedication. He spent countless hours researching and exploring various African cultures and traditions, drawing inspiration from ancient artifacts, folklore, and tribal rituals. He also sought the guidance of local elders and historians, who shared their knowledge and insights into the rich heritage of Kenya and its people.</p>
-        <p>The project took shape over the course of a year, with John meticulously creating each piece of artwork by hand. He experimented with various techniques and mediums, including oil paints, watercolors, and charcoal, to bring his vision to life. Each painting was a masterpiece in its own right, capturing the essence of African culture and conveying a sense of history and tradition.</p>
-        <p>When the project was finally completed, John held a grand exhibition that attracted art enthusiasts and critics from all over the country. The response was overwhelming, with many praising John's unique style and creative vision. His project not only showcased his artistic abilities but also served as a powerful reminder of the importance of preserving African heritage and identity.</p>
-        <p>Looking back on his project, John reflects on the inspiration and passion that drove him to create something truly remarkable. He hopes that his work will inspire others to explore their own cultural heritage and embrace the rich diversity of African traditions.</p>
+      <>
+        {
+          newData?.map((cardInfo) => {
+            return (
+              <ItemCard cardInfo={cardInfo} key={cardInfo.id} />
+            )
+          })
+        }
+      </>
+      <div className={styles.desc} style={{width: "450px"}}>
+        <p>Welcome to my website! I'm an artist who loves to work with mixed mediums, sculpturing, and content creation. My general style is inspired by the beauty and complexity of the world around me, and I strive to create art that evokes emotion and captures the imagination.<br/>
+        Working with mixed mediums allows me to experiment with different textures, colors, and forms. I love to combine traditional mediums like paint and ink with unconventional materials like paper, fabric, and found objects. The result is a unique and dynamic artwork that engages the senses and sparks curiosity.<br/>
+        Sculpturing is another passion of mine, and I enjoy working with a variety of materials, from clay and stone to metal and wood. Sculpting allows me to explore three-dimensional forms and bring my ideas to life in a tangible way. I enjoy the challenge of working with different materials and techniques, and I'm always pushing myself to learn and grow as an artist.<br/>
+        In addition to my traditional artwork, I also create digital content, including illustrations, animations, and videos. This allows me to reach a wider audience and share my art in new and exciting ways.
+        Overall, my style is characterized by a sense of playfulness and experimentation, and I'm always exploring new mediums and techniques. I believe that art should be accessible and enjoyable for everyone, and I strive to create artwork that is both aesthetically pleasing and thought-provoking.</p>
       </div>
     </div>
   )
