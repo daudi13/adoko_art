0d973942e55c26325ed1385808f2d38c9966116c
daudi13 <davouma13@gmail.com>
2023-04-08 13:55:37 +0300

    Add info button

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -6,6 +6,7 @@ const ItemCard = ({ cardInfo }) => {
   return (
     <div className={styles.cardImage}>
       <Image src={Img} alt="painting" width="450" height="600" className={styles.img} />
+      <button className={styles.infoButton}>info</button>
     </div>
   )
 }
