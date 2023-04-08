894b0bc5a36a34fea2ebc79d0ccbbf1a4ab5cb74
daudi13 <davouma13@gmail.com>
2023-04-08 21:37:33 +0300

    Add close button

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from "@/styles/ItemCard.module.scss"
-import { useState } from 'react'
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const ItemCard = ({ cardInfo }) => {
   const [active, setActive] = useState(false);
@@ -11,6 +12,7 @@ const ItemCard = ({ cardInfo }) => {
       <Image src={Img} alt="painting" width="450" height="600" className={styles.img} onClick={() => setActive(false)}/>
       <button className={styles.infoButton} onClick={() => setActive(true)}>info</button>
       <div className={active ? styles.desc : styles.none}>
+        <AiFillCloseCircle size={32} className={styles.close} onClick={() => setActive(false)}/>
         <p>{cardInfo.desc}</p>
       </div>
     </div>
