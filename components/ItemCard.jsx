487dc47aa44e37e0532c11f8f823198ab3ce10e7
daudi13 <davouma13@gmail.com>
2023-04-08 13:47:34 +0300

    Add classname for image

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -5,7 +5,7 @@ const ItemCard = ({ cardInfo }) => {
   const Img = cardInfo.image
   return (
     <div className={styles.cardImage}>
-      <Image src={Img} alt="painting" width="550"  />
+      <Image src={Img} alt="painting" width="450" height="600" className={styles.img} />
     </div>
   )
 }
