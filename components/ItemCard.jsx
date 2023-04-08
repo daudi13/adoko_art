3cc545afd909ea71deb3060f4ee1bde0f08eb144
daudi13 <davouma13@gmail.com>
2023-04-08 14:32:19 +0300

    Add useState logic

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import styles from "@/styles/ItemCard.module.scss"
+import { useState } from 'react'
 
 const ItemCard = ({ cardInfo }) => {
+  const [active, setActive] = useState(false);
+
   const Img = cardInfo.image
   return (
     <div className={styles.cardImage}>
-      <Image src={Img} alt="painting" width="450" height="600" className={styles.img} />
-      <button className={styles.infoButton}>info</button>
-      <div className={styles.desc}>
+      <Image src={Img} alt="painting" width="450" height="600" className={styles.img} onClick={() => setActive(false)}/>
+      <button className={styles.infoButton} onClick={() => setActive(true)}>info</button>
+      <div className={active ? styles.desc : styles.none}>
         <p>{cardInfo.desc}</p>
       </div>
     </div>
