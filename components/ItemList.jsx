4d5ed49a25dea83bb9f21c57995f7e182999c2f1
daudi13 <davouma13@gmail.com>
2023-04-10 13:07:16 +0300

    replace data with painting

--- components/ItemList.jsx
+++ components/ItemList.jsx
@@ -3,12 +3,12 @@ import data from "./data"
 import ItemCard from "./ItemCard"
 
 const ItemList = () => {
-  const newData = data
+  const {paintings} = data
   return (
     <div className={styles.container}>
       <>
         {
-          newData?.map((cardInfo) => {
+          paintings?.map((cardInfo) => {
             return (
               <ItemCard cardInfo={cardInfo} key={cardInfo.id} />
             )
