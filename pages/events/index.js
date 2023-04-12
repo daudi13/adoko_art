a56e7cd1218689e7586f37ee51ab40adacef3f74
daudi13 <davouma13@gmail.com>
2023-04-12 13:50:51 +0300

    Add overly

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,13 @@
+import styles from "@/styles/Events.module.scss"
+
 const index = () => {
   return (
-    <div>
-      <h2>No Upcoming events</h2>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h2>No Upcoming events</h2>
+      <div className={styles.overlay} />
+      </div>
+    </>
   )
 }
 
