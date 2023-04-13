1c89e49db5be64583a12ff68c530129c7c02be6e
daudi13 <davouma13@gmail.com>
2023-04-13 12:35:01 +0300

    Add shop page

---
+++ pages/shop/index.js
@@ -0,0 +1,14 @@
+import styles from "@/styles/Shop.module.scss"
+
+const index = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2>Online store coming soon</h2>
+      <div className={styles.overlay} />
+      </div>
+    </>
+  )
+}
+
+export default index
