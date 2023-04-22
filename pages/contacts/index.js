6689c2f4081b68ab1de665aaac14bd54226c3bd5
daudi13 <davouma13@gmail.com>
2023-04-22 22:40:50 +0300

    Import form

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -53,6 +53,21 @@ const index = () => {
       <div className={styles.contactForm}>
       </div>
       <div className={styles.overlay}></div>
+      <form className={styles.form}>
+        <div className={styles.formTags}>
+        <label for="subject">Subject</label>
+        <input type="text" id="subject" name="subject"/>
+        </div>
+        <div className={styles.formTags}>
+        <label for="email">Your email</label>
+        <input type="email" id="email" name="email" />
+        </div>
+        <div className={styles.formTags}>
+        <label for="content">content</label>
+        <textarea></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
