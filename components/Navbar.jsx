2cb0a6e9b403f22b884e59f53f3a47e98b792391
daudi13 <davouma13@gmail.com>
2023-05-08 08:13:10 +0300

    Import actions

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,18 @@
 import styles from "@/styles/Navbar.module.scss"
 import { RxHamburgerMenu } from "react-icons/rx"
 import NavSection from "./NavSection"
+import { useDispatch, useSelector } from "react-redux"
+import { open, close } from "@/redux/switchSlice";
+
+
 const Navbar = () => {
+  const switchValue = useSelector((state) => state.navSwitch.value);
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.nav}>
       <h1 className={styles.logo}>Adoko Arts</h1>
-      <NavSection/>
+      {/* <NavSection/> */}
       <RxHamburgerMenu size={45} className={styles.menuBtn} />
     </div>
   )
